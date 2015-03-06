4155e51eaf05f4afc2aaa86158b3a62561a4f90c
carlosmiguelpatino <cm.patino10@uniandes.edu.co>
2015-03-06 11:21:56 -0500

    version final

--- animate.py
+++ animate.py
@@ -1,25 +1,43 @@
 import os
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 
-os.makedirs("Graficas")
+
+
+
+col1 = []
+col2 = [] 
 
 for filename in os.listdir("./Test"):
 	
 	path  = "./Test/" + filename
 	data = np.loadtxt(path)
 
-	col1 = data[:,0]
-	col2 = data[:,1]
 
-	plt.plot(col1, col2)
-	plt.title(filename)
-	imagepath = filename[:-4]
-	imagepath = "./Graficas/"+ imagepath + ".png"
-	plt.savefig(imagepath)
+	col1tmp = data[:,0]
+	col2tmp = data[:,1]
+        
+
+        for i in range(len(col1tmp)):
+                
+                col1.append(float(col1tmp[i]))
+                col2.append(float(col2tmp[i]))
 
 
 
+fig = plt.figure()
+ax1 = fig.add_subplot(1,1,1)
+
+def animate(i):
+        x = col1[i]
+        y = col2[i]
+        ax1.clear()
+        ax1.scatter(x,y)
+
+anim = animation.FuncAnimation(fig, animate, interval=50, blit = True)
+
+plt.show()
 
 	
 
--- csvtodat.py
+++ csvtodat.py
@@ -2,6 +2,8 @@ infile = open("Notas.csv", 'r')
 outfile = open("Notas.dat", 'w')
 linea =  infile.readline()
 
+import os
+
 linea = linea.replace(',T' , '+T')
 #En esta version se escoge que el formato del encabezado sea Taller1 y no Taller 1. Sin embargo se presenta la linea de codigo necesaria para hacer lo contrario
 linea = linea.replace('r,',  'r')
@@ -15,4 +17,14 @@ for i in range(31):
 
 infile.close()
 outfile.close()
+
+#No se si en la segunda parte del punto tocaba hacer esto
+os.system('sed \'s/,T/!T/g\' Notas.csv > Notasterminaltmp1.dat')
+os.system('sed \'s/r,/r/g\' Notasterminaltmp1.dat > Notasterminaltmp2.dat')
+os.system('sed \'s/,/!/g\' Notasterminaltmp2.dat > Notasterminal.dat')
+os.system('rm Notasterminaltmp1.dat')
+os.system('rm Notasterminaltmp2.dat')
+
+
+
 
